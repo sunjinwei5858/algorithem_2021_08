00e02018b643854043cf8b827c96de9001884164
sunjinwei <sunjinwei5828@163.com>
2021-08-21 10:51:03 +0800

    feat: 组合总和I，力扣39，进行剪枝后 性能大大提升

---
+++ src/com/sunjinwei/huisu/Zuhe_Sum_01.java
@@ -0,0 +1,91 @@
+package com.sunjinwei.huisu;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 组合总和39
+ * 给定一个无重复元素的正整数数组candidates和一个正整数target，
+ * 找出candidates中所有可以使数字和为目标数target的唯一组合。
+ * candidates中的数字可以无限制重复被选取。
+ * 如果至少一个所选数字数量不同，则两种组合是唯一的。 
+ * 对于给定的输入，保证和为target的唯一组合数少于150 个。
+ */
+public class Zuhe_Sum_01 {
+
+    public List<List<Integer>> res = new ArrayList<>();
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        if (candidates == null || candidates.length == 0 || target <= 0) {
+            return res;
+        }
+        backTrack(candidates, target, 0, new LinkedList<>());
+        return res;
+    }
+
+    private void backTrack(int[] candidates, int target, int startIndex, LinkedList<Integer> path) {
+        // 终止条件1
+        if (target == 0) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        // 终止条件2
+        if (target < 0) {
+            return;
+        }
+        // 终止条件3
+        if (startIndex == candidates.length) {
+            return;
+        }
+        for (int i = startIndex; i < candidates.length; i++) {
+            path.add(candidates[i]);
+            // 可以无限制重复选取
+            backTrack(candidates, target - candidates[i], i, path);
+            path.removeLast();
+        }
+    }
+
+    /**
+     * 剪枝
+     *
+     * @param candidates
+     * @param target
+     * @return
+     */
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        if (candidates == null || candidates.length == 0 || target <= 0) {
+            return res;
+        }
+        backTrack2(candidates, target, 0, new LinkedList<>());
+        return res;
+    }
+
+    private void backTrack2(int[] candidates, int target, int startIndex, LinkedList<Integer> path) {
+        if (target == 0) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        if (startIndex == candidates.length) {
+            return;
+        }
+        for (int i = 0; i < candidates.length; i++) {
+            // 这里进行剪枝!!!!! 效率非常明显
+            if (candidates[i] > target) {
+                continue;
+            }
+            path.add(candidates[i]);
+            backTrack2(candidates, target - candidates[i], i, path);
+            path.removeLast();
+        }
+    }
+
+    public static void main(String[] args) {
+        Zuhe_Sum_01 zuhe02 = new Zuhe_Sum_01();
+        int[] arr = new int[]{2, 3, 5};
+        List<List<Integer>> res = zuhe02.combinationSum(arr, 8);
+        for (List<Integer> re : res) {
+            System.out.println(re.toString());
+        }
+    }
+}
